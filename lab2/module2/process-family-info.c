4d8ac183352ceafca8b31832f8554851eb10662d
yjhmelody <465402634@qq.com>
2017-10-25 18:54:28 +0800

    lab2:module2

--- lab2/module2/process-family-info.c
+++ lab2/module2/process-family-info.c
@@ -10,17 +10,16 @@
 MODULE_LICENSE("GPL");
 
 static int pid;
-
-module_param(pid, int, 0644);
+module_param(pid, int, 0755);
 
 static int hello_init(void)
 {
     struct task_struct *task = pid_task(find_vpid(pid), PIDTYPE_PID);
     struct task_struct *parent = task->parent;
-    struct task_struct *p;    
+    struct task_struct *p = NULL;
 
-    struct list_head *sibling;
-    struct list_head *child;
+    struct list_head *sibling = NULL;
+    struct list_head *child = NULL;
 
     printk(KERN_ALERT "父进程:%s,pid:%d\n", parent->comm, parent->pid);
     printk(KERN_ALERT "该进程:%s,pid:%d\n", task->comm, task->pid);
@@ -45,7 +44,7 @@ static int hello_init(void)
 
 static void hello_exit(void)
 {
-    printk(KERN_ALERT "good bye thread-family-info\n");
+    printk(KERN_ALERT "good bye process-family-info\n");
 }
 
 module_init(hello_init);
