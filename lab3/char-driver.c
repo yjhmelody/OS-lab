dc2a6cba7e32dc59f9e1854cd1d22a817ec987b0
yjhmelody <465402634@qq.com>
2017-11-17 12:09:49 +0800

    lab3: init the base struct

--- lab3/char-driver.c
+++ lab3/char-driver.c
@@ -11,16 +11,32 @@
 
 MODULE_LICENSE("GPL");
 
-// my char driver
+// yjh's char driver
 typedef struct yjh_mem_dev
 {
     struct cdev cdev;
     unsigned char mem[512];
 } yjh_mem_dev;
 
-static int driver_init(void)
-{
+// 这些函数具体实现设备的相关操作
+static int (*yjh_open)(struct inode *inode, struct file *file);
+static int (*yjh_read)(struct file *file, char __user *buf, size_t n, loff_t *loff);
+static int (*yjh_write)(struct file *file, const char __user *buf, size_t n, loff_t *loff);
+static int (*yjh_ioctl)(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg);
+static int (*yjh_release)(struct inode *inode, struct file *file);
 
+// file_operations 结构体中包含很多函数指针，是驱动程序与内核的接口
+static struct file_operations yjh_file_op = {
+    .owner = THIS_MODLUE,
+    .open = yjh_open,
+    .read = yjh_read,
+    .write = yjh_write,
+    .ioctl = yjh_ioctl,
+    .release = yjh_release}
+
+static int
+driver_init(void)
+{
 }
 
 static void driver_exit(void)
