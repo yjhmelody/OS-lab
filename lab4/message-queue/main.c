67e7f405e4a56580cb0a117d3aaec6adc8374c56
yjhmelody <465402634@qq.com>
2017-12-14 23:19:55 +0800

    update

--- lab4/message-queue/main.c
+++ lab4/message-queue/main.c
@@ -6,13 +6,18 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define MSG_LEN 80
+#define MSG_LEN 5123
 #define MSG_END 1
 
 pthread_t reciver_id;
 pthread_t sender_id;
-pthread_mutex_t mutex;
+
+sem_t sem_empty;
+sem_t sem_full;
+sem_t sem_end;
+
 int msg_id;
 
 typedef struct msg_data
@@ -21,89 +26,111 @@ typedef struct msg_data
     char text[MSG_LEN];
 } msg_data;
 
+#define MSG_DATA_SIZE (sizeof(msg_data))
 
-void check_error(int err)
+#define check_error(err)                                                 \
+    do                                                                   \
+    {                                                                    \
+        if (err != 0)                                                    \
+        {                                                                \
+            fprintf(stderr, "错误为第%d行,错误码为%d\n", __LINE__, err); \
+            exit(err);                                                   \
+        }                                                                \
+    } while (0);
+
+void *sender(void *args)
 {
-    if (err != 0)
+    msg_data data;
+    data.type = 0;
+
+    int len;
+    int err;
+    char str[MSG_LEN];
+
+    for (;;)
     {
-        printf("error:%d\n", err);
-        exit(err);
+        sem_wait(&sem_empty);
+        printf("sender: ");
+        scanf("%s", str);
+
+        if (strcmp(str, "exit") == 0)
+        {
+            break;
+        }
+        strcpy(data.text, str);
+        printf("your words: %s\n", data.text);
+        err = msgsnd(msg_id, &data, MSG_DATA_SIZE, 0);
+        check_error(err);
+
+        sem_post(&sem_full);
     }
+    printf("exit\n");
+    strcpy(data.text, "end");
+    err = msgsnd(msg_id, &data, MSG_DATA_SIZE, 0);
+    check_error(err);
+
+    sem_post(&sem_full);
+
+    // 收到结束
+    sem_wait(&sem_end);
+
+    len = msgrcv(msg_id, &data, MSG_DATA_SIZE, MSG_END, 0);
+    err = msgctl(msg_id, IPC_RMID, 0);
+    check_error(err);
+    
+    return (void *)0;
 }
 
 void *reciver(void *args)
 {
     msg_data data;
-    long msg_type = 0;
     int err;
+    int len;
+    data.type = 0;
+
     for (;;)
     {
-        err = msgrcv(msg_id, &data, MSG_LEN, msg_type, 0);
-        check_error(err);
+        // 不能交换
+        sem_wait(&sem_full);
 
-        // when meet "end"
+        len = msgrcv(msg_id, &data, MSG_DATA_SIZE, 0, 0);
         if (strcmp(data.text, "end") == 0)
         {
-            // send "over"
-            strcpy(data.text, "end");
-            data.type = MSG_END;
-            err = msgsnd(msg_id, &data, MSG_LEN, IPC_NOWAIT);
-            check_error(err);
+            printf("end\n");
             break;
         }
-
-        printf("%s\n", data.text);
+        printf("reciver: %s\n", data.text);
+        sem_post(&sem_empty);
     }
+    data.type = MSG_END;
+    strcpy(data.text, "over");
 
-    // 正常退出
-    return (void *)0;
-}
+    sem_post(&sem_empty);
 
-void *sender(void *args)
-{
-    msg_data data;
-    int len;
-    int err;
-    while (~scanf("%s", data.text))
-    {
-        len = strlen(data.text);
-        if (strcmp(data.text, "exit") == 0)
-        {
-            // send "end"
-            err = msgsnd(msg_id, &data, MSG_LEN, IPC_NOWAIT);
-            check_error(err);
+    // 通知结束
+    sem_post(&sem_end);
 
-            // wait for response
-            len = msgrcv(msg_id, &data, MSG_LEN, MSG_END, 0);
-            if (len == -1)
-            {
-                printf("error:%d\n", -1);
-                exit(-1);
-            }
-
-            printf("%s\n", data.text);
-            err = msgctl(msg_id, IPC_RMID, 0);
-            check_error(err);
-            break;
-        }
-
-        err = msgsnd(msg_id, &data, MSG_LEN, IPC_NOWAIT);
-        check_error(err);
-    }
+    err = msgsnd(msg_id, &data, MSG_DATA_SIZE, 0);
+    check_error(err);
+    
     return (void *)0;
 }
 
 int main()
 {
     int err;
+    sem_init(&sem_empty, 0, 2);
+    sem_init(&sem_full, 0, 0);
+    sem_init(&sem_end, 0, 0);
 
-    pthread_mutex_init(&mutex, NULL);
-    msg_id = msgget((key_t)1234, 0666 | IPC_CREAT);
+    msg_id = msgget(IPC_PRIVATE, 666 | IPC_CREAT);
 
     err = pthread_create(&reciver_id, NULL, reciver, NULL);
     check_error(err);
     err = pthread_create(&sender_id, NULL, sender, NULL);
     check_error(err);
-    sleep(30);
+
+    pthread_join(reciver_id, NULL);
+    pthread_join(sender_id, NULL);
     return 0;
 }
